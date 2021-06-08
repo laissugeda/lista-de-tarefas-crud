a00948c1b62529c2ee48d2155cb7575fd5ef735c
laissugeda <la_sugeda@hotmail.com>
2021-06-08 16:31:50 -0300

    improving edit function

--- script.js
+++ script.js
@@ -13,24 +13,29 @@ var ListaTarefas = new function () {
         }
     }
 
-    this.excluiTarefa = (posicao) => {
+    this.excluiTarefa = posicao => {
         this.tarefas.splice(posicao, 1)
         this.mostraTarefas()
     }
 
-    this.editaTarefa = (posicao) => {
+    this.editaTarefa = posicao => {
         document.getElementById('editar').type = 'text'
         document.getElementById('editar').value = this.tarefas[posicao]
-        salvar.innerHTML = '<input class="botao" style="background-color: rgb(29, 109, 29); color: white;" type="submit" value="Salvar" onclick="ListaTarefas.salvaEditaTarefa('+ posicao +')">'
+        salvar.innerHTML = '<input class="botao" style="background-color: rgb(29, 109, 29); color: white;" type="submit" value="Salvar" onclick="ListaTarefas.salvaEditaTarefa('+ posicao +')">' +
+                           '<strong style="cursor: pointer" onclick="ListaTarefas.fechaEditaTarefa()"> X </strong>'
     }
 
-    this.salvaEditaTarefa = (posicao) => {
+    this.salvaEditaTarefa = posicao => {
         var editada = document.getElementById('editar').value
             for(var i in this.tarefas){
                 if (i == posicao){
                     this.tarefas[i] = editada
                 }
             }
+        this.fechaEditaTarefa()
+    }
+
+    this.fechaEditaTarefa = () => {
         document.getElementById('editar').type = 'hidden'
         salvar.innerHTML= ''
         this.mostraTarefas()  
@@ -58,11 +63,11 @@ var ListaTarefas = new function () {
     }
     this.modoNoturno = () => {
         lua.innerHTML = '<input type="image" id="img" src="sol.png" class="imagem" onclick="ListaTarefas.modoDiurno()"></input>'
-        document.getElementById('body').style ='background-color: black;'
+        document.body.style ='background-color: black;'
     } 
     this.modoDiurno = () => {
         lua.innerHTML = '<input type="image" id="img" src="lua.png" class="imagem" onclick="ListaTarefas.modoNoturno()"></input>'
-        document.getElementById('body').style ='background-color: white;'
+        document.body.style ='background-color: white;'
     }
 }
  
